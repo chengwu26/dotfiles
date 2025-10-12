53c17fbc3d219a14d05e4a786806f492303f56fa
kanggui <kg.he@outlook.com>
2025-10-12 17:05:08 +0800

    feat(setup): rewrite setup.sh

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,39 +1,61 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ -f /etc/os-release ]]; then
-  source /etc/os-release
-  if [[ $ID != 'arch' ]]; then
-    echo "This script only supports Arch Linux, but $NAME"
-    exit 1
-  fi
-else
-  echo "Unknown OS"
-  exit 1
-fi
+set -e
+command -v pacman > /dev/null || { echo 'This script only supports Arch Linux'; exit 1; }
 
-apps=(
-  sudo tmux tree
-  zsh fzf zoxide
-  man-db man-pages
-  uv pyright rustup openssh
-  fastfetch ffmpeg bat hyperfine
-  neovim git tree-sitter-cli gcc yarn npm ripgrep fd unzip inotify-tools 
-)
-
-pacman-key --init
-pacman-key --populate archlinux
-
-pacman -Syyu --noconfirm "${apps[@]}"
-exec zsh
+function info() {
+  printf '\e[32m=== INFO: %s ===\e[0m\n' "$1"
+}
 
 function repo2url() {
   echo "https://github.com/chengwu26/${1}.git"
 }
 
+function gitclone() {
+  local repo="$1"
+  local dir="$2"
+  local bare="$3"
+
+  local cmd=(git clone)
+  [[ $bare == true ]] && cmd+=(--bare)
+  cmd+=("$(repo2url "$repo")" "$dir")
+
+  [[ ! -d "$dir" ]] && { "${cmd[@]}"; return;}
+  while true; do
+    read -p "The '$dir' already exists, do you want to override(y/n/q)? " opt
+    case $opt in
+      n) break;;
+      q) exit 2;;
+      y)
+        rm -rf "$dir"
+        "${cmd[@]}"
+        break
+        ;;
+      *) echo 'Invalid option';;
+    esac
+  done
+}
+
+declare -a apps=(
+  sudo man-db man-pages openssh base-devel
+  zsh fzf zoxide tmux tree fastfetch ffmpeg hyperfine ripgrep fd
+  uv pyright rustup git
+  neovim tree-sitter-cli yarn npm unzip inotify-tools
+)
+
+info 'Initialize pacman keyring'
+pacman-key --init
+pacman-key --populate archlinux
+
+info 'Install apps'
+pacman -Syu --noconfirm --needed "${apps[@]}"
+
+info 'Install rust toolchain'
 rustup toolchain install stable
 
-git clone --bare "$(repo2url dotfiles)" "${HOME}/.dotfiles"
-git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" reset --hard
-source "${HOME}/.zshrc"
+info 'Clone dotfiles repo'
+gitclone dotfiles ~/.dotfiles true
+git --git-dir=${HOME}/.dotfiles --work-tree=${HOME} reset --hard
 
-git clone "$(repo2url nvim)" "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+info 'Clone nvim repo'
+gitclone nvim "${HOME}/.config/nvim"
