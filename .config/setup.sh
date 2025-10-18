a0aa0655836c0bdcb4b57583446834cba850d8d6
kanggui <kg.he@outlook.com>
2025-10-18 17:26:03 +0800

    feat(setup): add tldr to apps

--- .config/setup.sh
+++ .config/setup.sh
@@ -26,7 +26,7 @@ function gitclone() {
 
 declare -a apps=(
   sudo man-db man-pages openssh base-devel unzip inotify-tools
-  fish fzf zoxide tmux tree fastfetch ffmpeg hyperfine ripgrep fd
+  fish fzf zoxide tmux tree fastfetch ffmpeg hyperfine ripgrep fd tldr
   clang lldb rustup uv pyright git
   neovim tree-sitter-cli yarn npm
 )
