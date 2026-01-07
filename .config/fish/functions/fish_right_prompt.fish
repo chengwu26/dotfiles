db63620e7f69e08b3a158a48b2c05f8c2677e4f2
kanggui <kg.he@outlook.com>
2026-01-07 22:56:51 +0800

    fix(shell): long autosuggestion conflicts with the right prompt
    In this solution, I turn off the right prompt. I will fix it in the
    future.

--- .config/fish/functions/fish_right_prompt.fish
+++
@@ -1,9 +0,0 @@
-function fish_right_prompt
-  if test $CMD_DURATION -gt 1000
-    set duration (math -s 2 $CMD_DURATION / 1000)
-    echo -n "$duration"s
-  else
-    echo -n "$CMD_DURATION"ms
-  end
-end
-
